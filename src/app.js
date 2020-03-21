d32e61e4e90cce9384187ef02c613c5f332aa14b
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-03-21 04:46:43 -0300

    feat: enable select into apollo or graphql server

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import { GRAPHQL_SERVER } from './config';
 import { authentication } from './middlewares';
 import graphQL from './graphql';
+import apolloServer from './apollo';
 import routes from './routes';
 
 const app = express();
@@ -14,8 +16,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(authentication);
 app.use(routes);
-app.use('/graphql', graphQL);
+
+if (GRAPHQL_SERVER === 'apollo') {
+  app.use(apolloServer.getMiddleware({ path: '/graphql' }));
+} else {
+  app.use('/graphql', authentication, graphQL);
+}
 
 export default app;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
-const { PORT = 4000, NODE_ENV = 'production', SECRET } = process.env;
+const { PORT = 4000, NODE_ENV = 'production', SECRET, GRAPHQL_SERVER = 'apollo' } = process.env;
 
 if (!SECRET) {
-  throw new Error('Environment var SECRET not defined');
+  throw new Error('Environment var "SECRET" not defined');
 }
 
-export { PORT, NODE_ENV, SECRET };
+const availableGraphQLServers = ['apollo', 'graphql'];
+if (!availableGraphQLServers.includes(GRAPHQL_SERVER)) {
+  throw new Error(
+    `Environment var "GRAPHQL_SERVER" invalid. Available values: ${availableGraphQLServers.join(
+      ', ',
+    )}`,
+  );
+}
+
+export { PORT, NODE_ENV, SECRET, GRAPHQL_SERVER };
