2a379e5c5b6e97ecbf15ad54e454cc2398210129
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-03-29 06:49:03 -0300

    feat: use db pool

--- src/components/users/stores/postgres.js
+++ src/components/users/stores/postgres.js
@@ -1,15 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import { Pool } from 'pg';
 
-import { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } from '../../../config';
-
-const client = new Pool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-});
+import client from '../../../pool';
 
 async function list({ limit, offset }) {
   const result = await client.query(
