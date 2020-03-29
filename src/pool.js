19669a296ada1e0a36cb95788c89cb63d27b6413
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-03-29 06:54:46 -0300

    chore: logger pool connect and remove

--- src/pool.js
+++ src/pool.js
@@ -1,6 +1,7 @@
 import { Pool } from 'pg';
 
 import { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } from './config';
+import logger from './logger';
 
 const client = new Pool({
   host: DB_HOST,
@@ -8,6 +9,14 @@ const client = new Pool({
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+});
+
+client.on('connect', () => {
+  logger.info('pool connect');
+});
+
+client.on('remove', () => {
+  logger.info('pool remove');
 });
 
 export default client;
